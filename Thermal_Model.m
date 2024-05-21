1050f7d14c25bcf91c13d82b867bcfcd15215ccc
EngsciBen <162936669+EngsciBen@users.noreply.github.com>
2024-05-21 19:37:22 +1200

    Update Thermal_Model.m

--- Thermal_Model.m
+++ Thermal_Model.m
@@ -1,6 +1,8 @@
 %Solving the Ode:
-P = 90;
-tspan = 0:0.1:90*60*20; %period of 90mins
+altitude = 500E3; %km
+Planet_radius = 6378E3; %
+P = 2*pi*sqrt((altitude + Planet_radius)^3/3.986004418E14);
+tspan = 0:0.1:P*20; %period of P*60 mins
 [t,T] = ode89(@Orbiting_Heat_Fluxes,tspan,273); %initial condition of 0K temp
 figure()
 plot(t, T)
