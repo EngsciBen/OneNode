b6f8e413b3229d8b4fefd8d91d2a4e2114786831
EngsciBen <162936669+EngsciBen@users.noreply.github.com>
2024-05-21 20:34:41 +1200

    Update Thermal_Model.m

--- Thermal_Model.m
+++ Thermal_Model.m
@@ -1,11 +1,19 @@
 %Solving the Ode:
-altitude = 500E3; %km
+altitude = 500E3; %
 Planet_radius = 6378E3; %
 P = 2*pi*sqrt((altitude + Planet_radius)^3/3.986004418E14);
-tspan = 0:0.1:P*20; %period of P*60 mins
+tspan = 0:0.1:P; %period of P*60 mins
 [t,T] = ode89(@Orbiting_Heat_Fluxes,tspan,273); %initial condition of 0K temp
+
+
+[dTdt, Qs, Qa, Qout] = Orbiting_Heat_Fluxes(tspan, 69);
+
 figure()
-plot(t, T)
+hold on
+%plot(t, T)
+plot(tspan, Qs)
+plot(tspan, Qa)
+plot(tspan, Qout)
 title('1 node CubeSat Thermal Model')
 xlabel('Time in orbit (s)')
 ylabel('Temperature (K)')
