db14191f8883d6726d34252235f267eb20d73eb7
EngsciBen <162936669+EngsciBen@users.noreply.github.com>
2024-05-22 14:22:27 +1200

    Update Thermal_Model.m

--- Thermal_Model.m
+++ Thermal_Model.m
@@ -1,19 +1,34 @@
 %Solving the Ode:
-altitude = 500E3; %
-Planet_radius = 6378E3; %
+altitude = 500E3; 
+Planet_radius = 6378E3; 
 P = 2*pi*sqrt((altitude + Planet_radius)^3/3.986004418E14);
-tspan = 0:0.1:P; %period of P*60 mins
+tspan = 0:100:P*20; %20 periods
 [t,T] = ode89(@Orbiting_Heat_Fluxes,tspan,273); %initial condition of 0K temp
 
+%[dTdt, Qs, Qa, Qout]
+dTdt = zeros(length(tspan));
+Qs = zeros(length(tspan));
+Qa = zeros(length(tspan));
+Qout = zeros(length(tspan));
 
-[dTdt, Qs, Qa, Qout] = Orbiting_Heat_Fluxes(tspan, 69);
+c = 0;
+for i = tspan
+    c = c + 1;
+    [dTdt(c), Qs(c) , Qa(c), Qout(c)] = Orbiting_Heat_Fluxes(i, 69);
+end
 
 figure()
-hold on
-%plot(t, T)
 plot(tspan, Qs)
+title('Solar Flux')
+xlabel('Time in orbit (s)')
+ylabel('Solar Flux (W)')
+figure()
 plot(tspan, Qa)
-plot(tspan, Qout)
+title('Albedo Flux')
+xlabel('Time in orbit (s)')
+ylabel('Albedo Flux (W)')
+figure()
+plot(t, T)
 title('1 node CubeSat Thermal Model')
 xlabel('Time in orbit (s)')
 ylabel('Temperature (K)')
