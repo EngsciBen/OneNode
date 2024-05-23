ef1b8ec2f350a1fbeaabab1c2cebc6de4b1329a1
EngsciBen <162936669+EngsciBen@users.noreply.github.com>
2024-05-23 22:18:34 +1200

    Update Thermal_Model.m

--- Thermal_Model.m
+++ Thermal_Model.m
@@ -2,19 +2,20 @@
 altitude = 500E3; 
 Planet_radius = 6378E3; 
 P = 2*pi*sqrt((altitude + Planet_radius)^3/3.986004418E14);
-tspan = 0:100:P*20; %20 periods
-[t,T] = ode89(@Orbiting_Heat_Fluxes,tspan,273); %initial condition of 0K temp
+tspan = 0:100:P*5; %20 periods
+[t,T] = ode89(@Orbiting_Heat_Fluxes,tspan,303.15); %initial condition of 0K temp
 
 %[dTdt, Qs, Qa, Qout]
 dTdt = zeros(length(tspan));
 Qs = zeros(length(tspan));
 Qa = zeros(length(tspan));
 Qout = zeros(length(tspan));
+QIR = zeros(length(tspan));
 
 c = 0;
 for i = tspan
     c = c + 1;
-    [dTdt(c), Qs(c) , Qa(c), Qout(c)] = Orbiting_Heat_Fluxes(i, 69);
+    [dTdt(c), Qs(c) , Qa(c), Qout(c), QIR(c)] = Orbiting_Heat_Fluxes(i, 303.15);
 end
 
 figure()
@@ -27,6 +28,21 @@ plot(tspan, Qa)
 title('Albedo Flux')
 xlabel('Time in orbit (s)')
 ylabel('Albedo Flux (W)')
+figure()
+plot(tspan, Qout)
+title('Out Flux')
+xlabel('Time in orbit (s)')
+ylabel('Out Flux (W)')
+figure()
+plot(tspan, QIR)
+title('IR Flux')
+xlabel('Time in orbit (s)')
+ylabel('IR Flux (W)')
+figure()
+plot(tspan, QIR)
+title('Qgen Flux')
+xlabel('Time in orbit (s)')
+ylabel('gen Flux (W)')
 figure()
 plot(t, T)
 title('1 node CubeSat Thermal Model')
